1d157bb3f6db29b7818405d7ea204df2eff57924
Andrew Ferguson <andrew.fergie@icloud.com>
2024-09-30 11:41:30 +0100

    save

--- autofeed.py
+++ autofeed.py
@@ -4,8 +4,20 @@
 
 totalWeight = 0
 containerWeights = []
-for i in range (5):
-    containerWeight = int(input(f"Please enter the weight of food in container {i+1}. If none, say '0': "))
-    while containerWeight<0 or containerWeight>200:
-        print("Please enter a valid weight in grams, 0-200.")
+dog_size = 0
+
+def get_food_weights():
+    for i in range (5):
         containerWeight = int(input(f"Please enter the weight of food in container {i+1}. If none, say '0': "))
+        while containerWeight<0 or containerWeight>200:
+            print("Please enter a valid weight in grams, 0-200.")
+            containerWeight = int(input(f"Please enter the weight of food in container {i+1}. If none, say '0': "))
+
+def enter_dog_size():
+    print("""Please enter your dog size.\n1 for small.\n2 for medium.\n3 for large.\n""")
+    dog_size = int(input("Either 1, 2, or 3: "))
+    while dog_size<0 or dog_size>3:
+        print("invalid answer, try again.")
+        dog_size = input("Either 1, 2, or 3: ")
+enter_dog_size()
+print(dog_size)
