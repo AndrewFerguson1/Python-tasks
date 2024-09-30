c6c0e11e0c986af982fd15c802d3607b565c8f24
Andrew Ferguson <andrew.fergie@icloud.com>
2024-09-30 10:36:04 +0100

    .

--- autofeed.py
+++ autofeed.py
@@ -1,1 +1,11 @@
-autofeed.py
+#autofeed
+#Andrew
+#27/09/2024
+
+totalWeight = 0
+containerWeights = []
+for i in range (5):
+    containerWeight = int(input(f"Please enter the weight of food in container {i+1}. If none, say '0': "))
+    while containerWeight<0 or containerWeight>200:
+        print("Please enter a valid weight in grams, 0-200.")
+        containerWeight = int(input(f"Please enter the weight of food in container {i+1}. If none, say '0': "))
